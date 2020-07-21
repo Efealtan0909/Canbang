3e6842212a96b6c776bf0b84f5b1e43b71cf8c88
Efealtan0909 <Efealtan0909@github.com>
2020-07-21 13:26:11 +0300

    Made Window

---
+++ index.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+Window = tk.Tk()
+
+button = tk.Button(
+    text="Click to Start the Game",
+    width=25,
+    height=5,
+    bg="green",
+    fg="white",
+)
+
+Window.mainloop()
