0d168f4f00789be3e60e0b4e86927640f9001213
Efealtan0909 <Efealtan0909@github.com>
2020-07-21 14:22:35 +0300

    core of move function and more TODO's

--- index.py
+++ index.py
@@ -3,6 +3,16 @@ import tkinter as tk
 Window = tk.Tk()
 Window.title('Canbang')
 
+WIDTH = HEIGHT = 400
+x1 = y1 = WIDTH / 2
+
+Canvas = tk.Canvas(
+    Window,
+    width=WIDTH,
+    height=HEIGHT
+)
+
+# TODO: make button start the game
 Button = tk.Button(
     text="Click to Start the Game",
     command="StartGame",
@@ -12,6 +22,31 @@ Button = tk.Button(
     fg="white",
 )
 
+# Functions
+
+# TODO: make moving work
+def move(event):
+    global x1, y1
+    if event.char == "w":
+    y1 += 1
+    pass
+    if event.char == "a":   
+    x1 += 1
+    pass
+    if event.char == "s":
+    y1 -= 1
+    pass
+    if event.char == "d":
+    x1 -= 1
+    pass
+pass
+# TODO: draw function
+# TODO: make movements work
+# TODO: add random can spawner
+
+
+
+canvas.pack()
 Button.pack()
 
 Window.mainloop()
