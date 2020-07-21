8b019bf5d5f2ff28c3c3e34b8b22310338f978a3
Efealtan0909 <Efealtan0909@github.com>
2020-07-21 14:03:19 +0300

    added title and made button display on the screen

--- index.py
+++ index.py
@@ -1,13 +1,17 @@
 import tkinter as tk
 
 Window = tk.Tk()
+Window.title('Canbang')
 
-button = tk.Button(
+Button = tk.Button(
     text="Click to Start the Game",
+    command="StartGame",
     width=25,
     height=5,
     bg="green",
     fg="white",
 )
+
+Button.pack()
 
 Window.mainloop()
